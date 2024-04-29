a966fa1725c0314e56566838d2b5092dc9def68f
Naoki Takahashi <43571743+nemuki@users.noreply.github.com>
2024-04-29 17:08:40 +0900

    トークン取得処理変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,56 @@
 import '@mantine/core/styles.css'
 
-import { MantineProvider } from '@mantine/core'
+import { Code, MantineProvider } from '@mantine/core'
+import { readLocalStorageValue, useLocalStorage } from '@mantine/hooks'
 import { OauthV2AccessResponse } from '@slack/web-api'
 import { useEffect, useState } from 'react'
 import { applicationConstants } from './constant.ts'
 import { fetchToken } from './slackApi.ts'
 
 function App() {
-  const [accessToken, setAccessToken] = useState<
+  const [slackOauthAccessToken, setSlackOauthAccessToken] = useState<
     OauthV2AccessResponse | undefined
   >(undefined)
+
+  const [localStorageSlackOauthToken, setLocalStorageSlackOauthToken] =
+    useLocalStorage<{
+      accessToken?: string
+      refreshToken?: string
+    }>({
+      key: 'slackOAuthToken',
+      defaultValue: readLocalStorageValue({ key: 'slackOAuthToken' }),
+    })
 
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(window.location.search)
     const oauthAuthorizationCode = urlSearchParams.get('code')
 
     if (oauthAuthorizationCode) {
-      console.info(oauthAuthorizationCode)
-
       fetchToken('authorization_code', oauthAuthorizationCode)
         .then((response) => {
-          console.info(response)
-          setAccessToken(response)
+          setLocalStorageSlackOauthToken({
+            accessToken: response.authed_user?.access_token,
+            refreshToken: response.authed_user?.refresh_token,
+          })
         })
         .catch((error) => {
           console.error(error)
         })
+
+      window.location.href = window.location.origin
+    } else {
+      const refreshToken = localStorageSlackOauthToken.refreshToken
+
+      if (refreshToken) {
+        fetchToken('refresh_token', refreshToken).then((response) => {
+          setLocalStorageSlackOauthToken({
+            accessToken: response.access_token,
+            refreshToken: response.refresh_token,
+          })
+
+          setSlackOauthAccessToken(response)
+        })
+      }
     }
   }, [])
 
@@ -33,13 +58,11 @@ function App() {
     <MantineProvider>
       <p>Hi</p>
       <a href={applicationConstants.slackOAuthAuthorizeUrl}>Slack</a>
-      <pre>
-        <code>
-          {accessToken
-            ? JSON.stringify(accessToken, undefined, 2)
-            : 'No access token'}
-        </code>
-      </pre>
+      <Code block>
+        {slackOauthAccessToken
+          ? JSON.stringify(slackOauthAccessToken, undefined, 2)
+          : 'No access token'}
+      </Code>
     </MantineProvider>
   )
 }
--- src/slackApi.ts
+++ src/slackApi.ts
@@ -4,21 +4,8 @@ import { env } from './env.ts'
 const slackApiBaseUrl = 'https://slack.com/api/'
 
 export async function fetchToken(
-  grantType: 'authorization_code',
-  token: string,
-  refreshToken?: never,
-): Promise<OauthV2AccessResponse>
-
-export async function fetchToken(
-  grantType: 'refresh_token',
-  refreshToken: string,
-  token?: never,
-): Promise<OauthV2AccessResponse>
-
-export async function fetchToken(
   grantType: 'authorization_code' | 'refresh_token',
-  code?: string,
-  refreshToken?: string,
+  token: string,
 ): Promise<OauthV2AccessResponse> {
   const params = new URLSearchParams()
   params.append('client_id', env.SLACK_CLIENT_ID)
@@ -28,18 +15,10 @@ export async function fetchToken(
 
   switch (grantType) {
     case 'authorization_code':
-      if (!code) {
-        throw new Error('Code is required when grantType is authorization_code')
-      }
-      params.append('code', code)
+      params.append('code', token)
       break
     case 'refresh_token':
-      if (!refreshToken) {
-        throw new Error(
-          'Refresh token is required when grantType is refresh_token',
-        )
-      }
-      params.append('refresh_token', refreshToken)
+      params.append('refresh_token', token)
       break
   }
 
