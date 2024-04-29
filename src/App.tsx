661095d35951db84c624f12fd5f3453c275cd9be
Naoki Takahashi <43571743+nemuki@users.noreply.github.com>
2024-04-29 14:35:54 +0900

    OAuth のトークン取得処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import { OauthV2AccessResponse } from '@slack/web-api'
+import { useEffect, useState } from 'react'
 import { applicationConstants } from './constant.ts'
+import { fetchToken } from './slackApi.ts'
 
 function App() {
+  const [accessToken, setAccessToken] = useState<
+    OauthV2AccessResponse | undefined
+  >(undefined)
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const oauthAuthorizationCode = params.get('code')
+
+    if (oauthAuthorizationCode) {
+      console.info(oauthAuthorizationCode)
+
+      fetchToken('authorization_code', oauthAuthorizationCode)
+        .then((response) => {
+          console.info(response)
+          setAccessToken(response)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  }, [])
+
   return (
     <>
       <p>Hi</p>
       <a href={applicationConstants.slackOAuthAuthorizeUrl}>Slack</a>
+      <pre>
+        <code>
+          {accessToken
+            ? JSON.stringify(accessToken, undefined, 2)
+            : 'No access token'}
+        </code>
+      </pre>
     </>
   )
 }
---
+++ src/slackApi.ts
@@ -0,0 +1,50 @@
+import { OauthV2AccessResponse } from '@slack/web-api'
+import { env } from './env.ts'
+
+export async function fetchToken(
+  grantType: 'authorization_code',
+  token: string,
+  refreshToken?: never,
+): Promise<OauthV2AccessResponse>
+
+export async function fetchToken(
+  grantType: 'refresh_token',
+  refreshToken: string,
+  token?: never,
+): Promise<OauthV2AccessResponse>
+
+export async function fetchToken(
+  grantType: 'authorization_code' | 'refresh_token',
+  code?: string,
+  refreshToken?: string,
+): Promise<OauthV2AccessResponse> {
+  const params = new URLSearchParams()
+  params.append('client_id', env.SLACK_CLIENT_ID)
+  params.append('client_secret', env.SLACK_CLIENT_SECRET)
+  params.append('redirect_uri', env.SLACK_REDIRECT_URI)
+  params.append('grant_type', grantType)
+
+  switch (grantType) {
+    case 'authorization_code':
+      if (!code) {
+        throw new Error('Code is required when grantType is authorization_code')
+      }
+      params.append('code', code)
+      break
+    case 'refresh_token':
+      if (!refreshToken) {
+        throw new Error(
+          'Refresh token is required when grantType is refresh_token',
+        )
+      }
+      params.append('refresh_token', refreshToken)
+      break
+  }
+
+  const response = await fetch('https://slack.com/api/oauth.v2.access', {
+    method: 'POST',
+    body: params,
+  })
+
+  return response.json()
+}
