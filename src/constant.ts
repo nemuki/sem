8d609e62082c0257b10c486af6e3b76c0e355455
Naoki Takahashi <43571743+nemuki@users.noreply.github.com>
2024-04-30 10:26:40 +0900

    リダイレクトが5173になってしまうためAuthorizedUrl に redirect_uri 追加

--- src/constant.ts
+++ src/constant.ts
@@ -4,6 +4,10 @@ const slackOAuthAuthorizeUrl = new URL('https://slack.com/oauth/v2/authorize')
 slackOAuthAuthorizeUrl.searchParams.append('client_id', env.SLACK_CLIENT_ID)
 slackOAuthAuthorizeUrl.searchParams.append('scope', '')
 slackOAuthAuthorizeUrl.searchParams.append(
+  'redirect_uri',
+  env.SLACK_REDIRECT_URI,
+)
+slackOAuthAuthorizeUrl.searchParams.append(
   'user_scope',
   'channels:history,channels:read,users.profile:read,users.profile:write,chat:write',
 )
