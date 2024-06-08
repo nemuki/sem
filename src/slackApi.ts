da0bb07696d946d1cd845b0188f81076347195cf
Naoki Takahashi <43571743+nemuki@users.noreply.github.com>
2024-06-08 11:27:24 +0900

    SearchParams を初期値で入れる

--- src/slackApi.ts
+++ src/slackApi.ts
@@ -13,11 +13,12 @@ export async function fetchToken(
   grantType: 'authorization_code' | 'refresh_token',
   token: string,
 ): Promise<OauthV2AccessResponse> {
-  const params = new URLSearchParams()
-  params.append('client_id', env.SLACK_CLIENT_ID)
-  params.append('client_secret', env.SLACK_CLIENT_SECRET)
-  params.append('redirect_uri', env.SLACK_REDIRECT_URI)
-  params.append('grant_type', grantType)
+  const params = new URLSearchParams({
+    client_id: env.SLACK_CLIENT_ID,
+    client_secret: env.SLACK_CLIENT_SECRET,
+    redirect_uri: env.SLACK_REDIRECT_URI,
+    grant_type: grantType,
+  })
 
   switch (grantType) {
     case 'authorization_code':
